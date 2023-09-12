59d2d41b95d68855b11709be48807d8721f1e308
konnorrogers <konnor5456@gmail.com>
2023-09-12 14:00:39 -0400

    prettier

--- docs/_utilities/replacer.cjs
+++ docs/_utilities/replacer.cjs
@@ -13,7 +13,9 @@
  * @param {Replacements} replacements
  */
 module.exports = function (content, replacements) {
+  /** This seems trivial, but by assigning to a string first, THEN using innerHTML after iterating over every replacement, we reduce the calculations of JSDOM. At the time of writing benchmarks show a reduction from 9seconds to 3 seconds by doing so. */
   let html = content.body.innerHTML;
+
   replacements.forEach(replacement => {
     html = html.replaceAll(replacement.pattern, replacement.replacement);
   });
