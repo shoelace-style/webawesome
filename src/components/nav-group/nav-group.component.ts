81ff1422e8dd3e1ce97c1d07007aa8f0acc553f3
konnorrogers <konnor5456@gmail.com>
2023-09-22 14:26:25 -0400

    working on nav group / nav-items

--- src/components/nav-group/nav-group.component.ts
+++ src/components/nav-group/nav-group.component.ts
@@ -4,6 +4,7 @@ import styles from './nav-group.styles.js';
 import type { CSSResultGroup } from 'lit';
 import { when } from 'lit/directives/when.js';
 import { property } from 'lit/decorators.js';
+import { LocalizeController } from '../../utilities/localize.js';
 import WaDetails from '../details/details.component.js';
 
 /**
@@ -21,6 +22,8 @@ export default class WaNavGroup extends WebAwesomeElement {
     'wa-details': WaDetails
   }
 
+  private readonly localize = new LocalizeController(this)
+
   /**
    * The label to display either as the summary for <wa-details> or above the <wa-nav-items>
    */
@@ -28,17 +31,38 @@ export default class WaNavGroup extends WebAwesomeElement {
   @property({ reflect: true, type: Boolean }) expandable: boolean = false
 
   render() {
-    return html`<div class="base" part="base" role="list">
+    const isRtl = this.localize.dir() === 'rtl';
+
+    return html`<div class="base nav-items" part="base" role="list">
       ${when(this.expandable,
         () => html`
-          <wa-details>
+          <wa-details
+            class="details"
+            part="details"
+            exportparts="
+              base:details__base,
+              header:details__header,
+              summary:details__summary,
+              summary-icon:details__summary-icon,
+              content:details__content
+            "
+          >
             <div slot="summary">
               <slot name="label">
                 ${this.label}
               </slot>
             </div>
 
-            <slot></slot>
+            <slot slot="expand-icon" name="expand-icon">
+              <wa-icon library="system" name=${isRtl ? 'chevron-left' : 'chevron-right'}></wa-icon>
+            </slot>
+            <slot slot="collapse-icon" name="collapse-icon">
+              <wa-icon library="system" name=${isRtl ? 'chevron-left' : 'chevron-right'}></wa-icon>
+            </slot>
+
+            <div class="nav-items" part="nav-items">
+              <slot></slot>
+            </div>
           </wa-details>
         `,
         () => html`<slot></slot>`
--- src/components/nav-group/nav-group.styles.ts
+++ src/components/nav-group/nav-group.styles.ts
@@ -9,11 +9,33 @@ export default css`
   }
 
   .base {
-    padding: var(--wa-space-square-s);
+    color: var(--wa-color-neutral-text-on-surface);
+  }
+
+  .nav-items {
     display: grid;
     align-content: start;
     grid-template-columns: minmax(0, 1fr);
     grid-template-rows: minmax(0, 1fr);
     gap: 6px;
+  }
+
+  .details::part(base) {
+    border: none;
+  }
+
+  .details::part(content) {
+    padding-top: 6px;
+  }
+
+  .details::part(header) {
+    padding: var(--wa-space-square-s);
+    border-radius: var(--wa-panel-corners);
+  }
+
+  .details::part(header):hover {
+    background-color: color-mix(in oklab, var(--wa-color-neutral-fill-vivid), var(--wa-color-tint-hover));
+    border-color: color-mix(in oklab, var(--wa-color-neutral-fill-vivid), var(--wa-color-tint-hover));
+    color: var(--wa-color-neutral-text-on-vivid);
   }
 `;
