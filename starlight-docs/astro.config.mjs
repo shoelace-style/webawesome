98e138ba34fb60ccc9ddb4c6c08d34bbf96d5927
konnorrogers <konnor5456@gmail.com>
2023-12-15 16:51:15 -0500

    converting to starlight

--- starlight-docs/astro.config.mjs
+++ starlight-docs/astro.config.mjs
@@ -6,31 +6,11 @@ export default defineConfig({
   server: {
     open: true
   },
-
-  markdown: {
-    shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'dracula',
-      // Alternatively, provide multiple themes
-      // https://github.com/antfu/shikiji#lightdark-dual-themes
-      experimentalThemes: {
-        light: 'github-light',
-        dark: 'github-dark',
-      },
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: [],
-      // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
-    },
-  },
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'WebAwesome',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/shoelace-style/shoelace',
 			},
 			sidebar: [
         {
