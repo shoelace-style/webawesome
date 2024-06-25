76d636453495aafd7758a71552ce2f0659089207
konnorrogers <konnor5456@gmail.com>
2024-06-25 14:55:15 -0400

    move top level

--- docs/assets/scripts/turbo.js
+++ docs/assets/scripts/turbo.js
@@ -1,48 +1,46 @@
-;(() => {
-  if (!window.___turboScrollPositions___) {
-    window.___turboScrollPositions___ = {};
-  }
+if (!window.___turboScrollPositions___) {
+  window.___turboScrollPositions___ = {};
+}
 
-  const positions = window.___turboScrollPositions___;
+const positions = window.___turboScrollPositions___;
 
-  function saveScrollPosition() {
-    document.querySelectorAll('[data-remember-scroll]').forEach(element => {
-      if (element.id) {
-        positions[element.id] = {
-          top: element.scrollTop,
-          left: element.scrollLeft
-        }
-      } else {
-        console.warn(`Can't save scroll position for elements without an id.`, el);
+function saveScrollPosition() {
+  document.querySelectorAll('[data-remember-scroll]').forEach(element => {
+    if (element.id) {
+      positions[element.id] = {
+        top: element.scrollTop,
+        left: element.scrollLeft
       }
+    } else {
+      console.warn(`Can't save scroll position for elements without an id.`, el);
+    }
+  });
+}
+
+function restoreScrollPosition(event) {
+  if (event.detail && event.detail.newBody) {
+    event.detail.newBody.querySelectorAll('[data-remember-scroll]').forEach(element => {
+      if (!positions[element.id]) { return }
+
+      const { top, left } = positions[element.id]
+
+      element.scrollTop = top
+      element.scrollLeft = left
     });
   }
 
-  function restoreScrollPosition(event) {
-    if (event.detail && event.detail.newBody) {
-      event.detail.newBody.querySelectorAll('[data-remember-scroll]').forEach(element => {
-        if (!positions[element.id]) { return }
+  requestAnimationFrame(() => {
+    document.querySelectorAll('[data-remember-scroll]').forEach(element => {
+      if (!positions[element.id]) { return }
 
-        const { top, left } = positions[element.id]
+      const { top, left } = positions[element.id]
 
-        element.scrollTop = top
-        element.scrollLeft = left
-      });
-    }
+      element.scrollTop = top;
+      element.scrollLeft = left;
+    });
+  })
+}
 
-    requestAnimationFrame(() => {
-      document.querySelectorAll('[data-remember-scroll]').forEach(element => {
-        if (!positions[element.id]) { return }
-
-        const { top, left } = positions[element.id]
-
-        element.scrollTop = top;
-        element.scrollLeft = left;
-      });
-    })
-  }
-
-  window.addEventListener('turbo:before-cache', saveScrollPosition);
-  window.addEventListener('turbo:before-render', restoreScrollPosition);
-  window.addEventListener('turbo:render', restoreScrollPosition);
-})();
+window.addEventListener('turbo:before-cache', saveScrollPosition);
+window.addEventListener('turbo:before-render', restoreScrollPosition);
+window.addEventListener('turbo:render', restoreScrollPosition);
