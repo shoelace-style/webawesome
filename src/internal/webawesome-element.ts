1bc84e7416d1a14156ffa9bf43ff0ed1e5eebf46
Lea Verou <lea@verou.me>
2025-01-06 22:02:48 -0500

    CSS properties to set component attributes
    - Base class abstraction
    - Use in `<wa-icon>` (docs excluded from alpha)

--- src/internal/webawesome-element.ts
+++ src/internal/webawesome-element.ts
@@ -1,7 +1,18 @@
-import type { CSSResult, CSSResultGroup, PropertyValues } from 'lit';
+import type { CSSResult, CSSResultGroup, PropertyDeclaration, PropertyValues } from 'lit';
 import { LitElement, isServer, unsafeCSS } from 'lit';
 import { property } from 'lit/decorators.js';
 import componentStyles from '../styles/shadow/component.css';
+import { getComputedStyle } from './computedStyle.js';
+
+// Augment Lit's module
+declare module 'lit' {
+  interface PropertyDeclaration {
+    /**
+     * Indicates whether the property should reflect to a CSS custom property.
+     */
+    cssProperty?: true | string;
+  }
+}
 
 export default class WebAwesomeElement extends LitElement {
   constructor() {
@@ -51,6 +62,18 @@ export default class WebAwesomeElement extends LitElement {
   initialReflectedProperties: Map<string, unknown> = new Map();
 
   internals: ElementInternals;
+
+  #computedStyle: CSSStyleDeclaration | null;
+  #setVia: Record<PropertyKey, 'css' | 'attribute' | 'js'> = {};
+  #setting = new Set<PropertyKey>();
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    // FIXME this is currently static.
+    // It will only update when the element is connected, not when a relevant CSS property changes.
+    this.updateCSSProperties();
+  }
 
   attributeChangedCallback(name: string, oldValue: string | null, newValue: string | null) {
     if (!this.#hasRecordedInitialProperties) {
@@ -111,6 +134,21 @@ export default class WebAwesomeElement extends LitElement {
     }
   }
 
+  updated(changedProperties: PropertyValues<this>) {
+    super.updated(changedProperties);
+
+    let Self = this.constructor as typeof WebAwesomeElement;
+
+    if (Self.cssAttributeProperties.size > 0) {
+      for (let [name] of changedProperties) {
+        if (typeof name === 'string' && this.#setVia[name] === 'css' && !this.#setting.has(name)) {
+          // A property is being set via JS and it’s NOT because we're reflecting a CSS property
+          this.#setVia[name] = 'js';
+        }
+      }
+    }
+  }
+
   /** Checks if states are supported by the element */
   private hasStatesSupport(): boolean {
     return Boolean(this.internals?.states);
@@ -147,5 +185,50 @@ export default class WebAwesomeElement extends LitElement {
   /** Determines if the element has the specified custom state. */
   hasCustomState(state: string): boolean {
     return this.hasStatesSupport() ? this.internals.states.has(state) : false;
+  }
+
+  protected updateCSSProperties() {
+    const Self = this.constructor as typeof WebAwesomeElement;
+    if (Self.cssAttributeProperties.size === 0) {
+      return;
+    }
+
+    this.#computedStyle ??= getComputedStyle(this);
+
+    const tagName = this.tagName.toLowerCase();
+    for (let [name, cssProperty] of Self.cssAttributeProperties) {
+      if (typeof name === 'string' && !this.hasAttribute(name) && this.#setVia[name] !== 'js') {
+        // Check if supplied as a CSS custom property
+        // TODO !important should override attribute values
+        cssProperty = cssProperty === true ? `--${tagName}-${name}` : cssProperty;
+        const value = this.#computedStyle?.getPropertyValue(cssProperty);
+
+        if (value) {
+          this.#setVia[name] = 'css';
+          this.#setting.add(name);
+          // @ts-ignore
+          this[name] = value.trim();
+          this.updateComplete.then(() => {
+            this.#setting.delete(name);
+          });
+        }
+      }
+    }
+  }
+
+  // Subclasses will get their own copy automagically (see below)
+  protected static cssAttributeProperties = new Map<PropertyKey, true | string>();
+
+  static createProperty(name: PropertyKey, options?: PropertyDeclaration): void {
+    super.createProperty(name, options);
+
+    if (options?.cssProperty) {
+      if (this.cssAttributeProperties === WebAwesomeElement.cssAttributeProperties) {
+        // Each class needs its own, otherwise they'd share the same object
+        this.cssAttributeProperties = new Map();
+      }
+
+      this.cssAttributeProperties.set(name, options.cssProperty);
+    }
   }
 }
