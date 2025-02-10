9873a9a925a6ec3da68b336d7741f2f6706098ba
Lea Verou <lea@verou.me>
2025-02-10 17:10:21 -0500

    WIP get palette code

--- docs/assets/scripts/remix.js
+++ docs/assets/scripts/remix.js
@@ -8,6 +8,13 @@ export const urls = {
   typography: id => `styles/themes/${id}/typography.css`,
 };
 
+export const selectors = {
+  palette: id => `:where(:root),
+  :host,
+  :where([class^='wa-theme-'], [class*=' wa-theme-']),
+  .wa-palette-${id}`,
+};
+
 function getImport(url, options = {}) {
   let { language = 'html', cdnUrl = '/dist/', attributes } = options;
   url = cdnUrl + url;
@@ -34,4 +41,52 @@ export function getCode(base, params, options) {
   );
 
   return ret.map(url => getImport(url, options)).join('\n');
+}
+
+export function getPaletteCode(palette, tweaks, options) {
+  let imports = [];
+
+  if (palette) {
+    imports.push(urls.palette(palette));
+  }
+
+  let css = '';
+
+  if (tweaks) {
+    let { hueShifts } = tweaks;
+    let declarations = [];
+
+    if (hueShifts) {
+      let element = document.querySelector(`.wa-palette-${palette}`) ?? document.documentElement;
+      let cs = getComputedStyle(element);
+
+      for (let hue in hueShifts) {
+        let shift = hueShifts[hue];
+
+        for (let suffix of ['', '-05', '-10', '-20', '-30', '-40', '-50', '-60', '-70', '-80', '-90', '-95']) {
+          let baseColor = cs.getPropertyValue(`--wa-color-${hue}${suffix}`);
+          declarations.push(`--wa-color-${hue}: oklch(from ${baseColor} l c calc(h - ${shift}));`);
+        }
+
+        declarations.push('');
+      }
+    }
+
+    css += `
+${selectors.palette} {
+  ${declarations.join('\n')}
+}`;
+  }
+
+  let ret = imports.map(url => getImport(url, options)).join('\n');
+
+  if (css) {
+    if (options.language === 'css') {
+      ret += `\n\n${css}`;
+    } else {
+      ret += `\n<style>\n${css}\n</style>`;
+    }
+  }
+
+  return ret;
 }
