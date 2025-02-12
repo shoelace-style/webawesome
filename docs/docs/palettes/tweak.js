48cf8b104db09dddb57a2f47299cc53c11c4e5ec
Lea Verou <lea@verou.me>
2025-02-12 14:46:16 -0500

    Make sure it works on Turbo

--- docs/docs/palettes/tweak.js
+++ docs/docs/palettes/tweak.js
@@ -3,51 +3,44 @@ import Color from 'https://colorjs.io/dist/color.js';
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 
 import { cdnUrl, getPaletteCode, hueRanges, hues, Permalink, tints } from '../../assets/scripts/tweak.js';
-import { palette } from '../../assets/scripts/tweak/code.js';
 import Prism from '/assets/scripts/prism.js';
-
-let codeSnippets = document.querySelector('#usage ~ wa-tab-group.import-stylesheet-code:first-of-type');
-codeSnippets = {
-  html: codeSnippets.querySelector('code.language-html'),
-  css: codeSnippets.querySelector('code.language-css'),
-};
-
-const paletteId = wa_data.paletteId;
-const permalink = new Permalink();
-const hueShifts = Object.fromEntries(hues.map(hue => [hue, 0]));
-const originalColors = wa_data.colors;
-
-// Replace colors with their oklch coords (since they're all opaque and all in the same color space)
-for (let hue in originalColors) {
-  for (let tint of tints) {
-    originalColors[hue][tint] = originalColors[hue][tint].coords;
-  }
-}
-
-// Read URL params and apply them. This facilitates permalinks.
-permalink.mapObject(hueShifts, {
-  keyTo: key => key.replace(/-shift$/, ''),
-  keyFrom: key => key + '-shift',
-  valueFrom: value => (!value ? '' : Number(value)),
-  valueTo: value => (!value ? 0 : Number(value)),
-});
-
-if (location.search) {
-  // Update from URL
-  permalink.writeTo(hueShifts);
-}
 
 await Promise.all(['wa-slider'].map(tag => customElements.whenDefined(tag)));
 
-globalThis.paletteApp = createApp({
+let paletteAppSpec = {
   data() {
+    const { paletteId, colors } = wa_data;
+
+    // Replace colors with their oklch coords (since they're all opaque and all in the same color space)
+    for (let hue in colors) {
+      for (let tint of tints) {
+        colors[hue][tint] = colors[hue][tint].coords;
+      }
+    }
+
     return {
+      permalink: new Permalink(),
       hueRanges,
-      hueShifts,
+      hueShifts: Object.fromEntries(hues.map(hue => [hue, 0])),
       paletteId,
-      originalColors,
+      originalColors: colors,
       tweaking: {},
     };
+  },
+
+  created() {
+    // Read URL params and apply them. This facilitates permalinks.
+    this.permalink.mapObject(this.hueShifts, {
+      keyTo: key => key.replace(/-shift$/, ''),
+      keyFrom: key => key + '-shift',
+      valueFrom: value => (!value ? '' : Number(value)),
+      valueTo: value => (!value ? 0 : Number(value)),
+    });
+
+    if (location.search) {
+      // Update from URL
+      this.permalink.writeTo(this.hueShifts);
+    }
   },
 
   mounted() {
@@ -96,26 +89,28 @@ globalThis.paletteApp = createApp({
   watch: {
     // Note: These could move to `v-html` directives if we widen the app root
     paletteHTML() {
-      codeSnippets.html.textContent = this.paletteHTML;
-      let copyButton = codeSnippets.html.previousElementSibling;
+      let codeElement = document.querySelector('#usage ~ wa-tab-group.import-stylesheet-code code.language-html');
+      codeElement.textContent = this.paletteHTML;
+      let copyButton = codeElement.previousElementSibling;
       copyButton.value = this.paletteHTML;
-      Prism.highlightElement(codeSnippets.html);
+      Prism.highlightElement(codeElement);
     },
 
     paletteCSS() {
-      codeSnippets.css.textContent = this.paletteCSS;
-      let copyButton = codeSnippets.css.previousElementSibling;
+      let codeElement = document.querySelector('#usage ~ wa-tab-group.import-stylesheet-code code.language-css');
+      codeElement.textContent = this.paletteCSS;
+      let copyButton = codeElement.previousElementSibling;
       copyButton.value = this.paletteCSS;
-      Prism.highlightElement(codeSnippets.css);
+      Prism.highlightElement(codeElement);
     },
 
     hueShifts: {
       deep: true,
       handler() {
-        permalink.readFrom(this.hueShifts);
+        this.permalink.readFrom(this.hueShifts);
 
         // Update page URL
-        permalink.updateLocation();
+        this.permalink.updateLocation();
 
         // Update contrast colors
         updateContrastTables(this.colors);
@@ -126,7 +121,11 @@ globalThis.paletteApp = createApp({
   compilerOptions: {
     isCustomElement: tag => tag.startsWith('wa-'),
   },
-}).mount('table.colors.main');
+};
+
+function init() {
+  globalThis.paletteApp = createApp(paletteAppSpec).mount('table.colors.main');
+}
 
 function updateContrastTables(colors) {
   for (let td of document.querySelectorAll('.contrast-table td[data-tint-bg][data-tint-fg]')) {
@@ -153,3 +152,6 @@ function updateContrastTables(colors) {
     swatch.classList.toggle('contrast-fail', contrast < minContrast);
   }
 }
+
+init();
+addEventListener('turbo:render', init);
