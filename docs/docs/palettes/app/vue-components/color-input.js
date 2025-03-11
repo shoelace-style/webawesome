fd9235fe296e7136088a10433ad22063380fd8c5
Lea Verou <lea@verou.me>
2025-03-10 20:01:06 -0400

    Make color identification more clearly output

--- docs/docs/palettes/app/vue-components/color-input.js
+++ docs/docs/palettes/app/vue-components/color-input.js
@@ -33,11 +33,11 @@ export default {
     },
   },
   template: `
-    <wa-card class="color" size="small">
+    <wa-card class="color" :class="colorInfo.level < 70 ? 'dark' : 'light'" size="small">
       <div slot="image" :style="{'--color': modelValue, colorScheme: colorInfo.level <= 60 ? 'dark' : 'light'}">
         <wa-icon-button name="trash" label="Delete" variant="regular" class="delete-button" @click="$emit('delete')"></wa-icon-button>
+        <div class="name">{{ capitalize(colorInfo.hue) || 'New color' }} {{ colorInfo.level }}</div>
       </div>
-      <div class="name">{{ capitalize(colorInfo.hue) || 'New color' }} {{ colorInfo.level }}</div>
       <wa-input :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"></wa-input>
     </wa-card>
   `,
