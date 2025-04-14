47b4f6d0e576b3807fe85141304ef7c21de433ca
Lea Verou <lea@verou.me>
2025-04-14 10:31:18 -0400

    Docs

--- docs/assets/scripts/util/dom-change.js
+++ docs/assets/scripts/util/dom-change.js
@@ -6,7 +6,13 @@ if (!initialPageLoadComplete) {
   });
 }
 
-// Helper for view transitions
+/**
+ * Helper for performing a DOM change using a view transition, wherever supported and reduced motion is not desired.
+ * @param {function} fn - Function to perform the DOM change. If async, must resolve when the change is complete.
+ * @param {object} [options] - Options for the transition
+ * @param {'smooth' | 'instant'} [options.behavior] - Transition behavior. Defaults to 'smooth'. 'instant' will skip the transition.
+ * @param {boolean} [options.ignoreInitialLoad] - If true, will skip the transition on initial page load. Defaults to true.
+ */
 export function domChange(fn, { behavior = 'smooth', ignoreInitialLoad = true } = {}) {
   const canUseViewTransitions =
     document.startViewTransition && !window.matchMedia('(prefers-reduced-motion: reduce)').matches;
