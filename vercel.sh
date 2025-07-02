eb5de1f9091457d47cbcee4974a9eae209bcf8b5
Tobias Bannwart <tobias.bannwart@bison-group.com>
2025-07-02 13:48:05 +0200

    vercel deployment

--- vercel.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Running the build!"
-git clone "https://konnorrogers:$GITHUB_ACCESS_TOKEN@github.com/shoelace-style/webawesome-pro" packages/webawesome-pro
-cd packages/webawesome-pro && npm run build
