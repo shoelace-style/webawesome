238811ac3102a349c5896302c7b37b92ceac6f89
konnorrogers <konnor5456@gmail.com>
2025-07-18 14:39:26 -0400

    only log last line

--- packages/webawesome/scripts/docs.js
+++ packages/webawesome/scripts/docs.js
@@ -119,7 +119,7 @@ export async function generateDocs(options = {}) {
           return !line.includes('Watching');
         });
         const lastLine = info[info.length - 1];
-        output = chalk.gray(`(${info.join('')})`);
+        output = chalk.gray(`(${lastLine})`);
         eleventy.logger.logger.reset();
       }
     } else {
