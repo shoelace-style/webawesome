2b69fa74a57f00bb11a07e2393fb7adf9737f869
konnorrogers <konnor5456@gmail.com>
2025-12-16 13:21:23 -0500

    Bump package.json version

--- packages/webawesome/scripts/update-root-version.js
+++ packages/webawesome/scripts/update-root-version.js
@@ -10,8 +10,8 @@ const monorepoRoot = path.resolve(__dirname, '..', '..', '..');
 const rootPackageJSONFile = path.join(monorepoRoot, 'package.json');
 const webawesomePackageJSONFile = path.join(path.resolve(__dirname, '..'), 'package.json');
 
-const rootPackageJSON = JSON.parse(fs.readFileSync(rootPackageJSONFile));
-const webawesomePackageJSON = JSON.parse(fs.readFileSync(webawesomePackageJSONFile));
+const rootPackageJSON = JSON.parse(fs.readFileSync(rootPackageJSONFile, { encoding: "utf8" }));
+const webawesomePackageJSON = JSON.parse(fs.readFileSync(webawesomePackageJSONFile, { encoding: "utf8" }));
 
 const currentVersion = webawesomePackageJSON.version;
 rootPackageJSON.version = currentVersion;
@@ -19,9 +19,9 @@ rootPackageJSON.version = currentVersion;
 fs.writeFileSync(rootPackageJSONFile, JSON.stringify(rootPackageJSON, null, 2));
 
 const versionsFile = path.join(monorepoRoot, 'VERSIONS.txt');
-const versions = fs.readFileSync(versions).split(/\r?\n/);
+const versions = fs.readFileSync(versionsFile, { encoding: "utf8" }).split(/\r?\n/);
 
 // TODO: Make this smart and understand semver and "insert" in the correct spot instead of appending.
 if (!versions.includes(currentVersion)) {
-  fs.appendFileSync(webawesomePackageJSON.version);
+  fs.appendFileSync(versionsFile, webawesomePackageJSON.version);
 }
