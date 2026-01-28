7381946875f9310576515fa50130f959a3eddb99
Cory LaViska <cory@abeautifulsite.net>
2026-01-28 08:58:02 -0500

    mark internal event as internal

--- packages/webawesome/src/internal/webawesome-element.ts
+++ packages/webawesome/src/internal/webawesome-element.ts
@@ -148,7 +148,7 @@ export default class WebAwesomeElement extends LitElement {
   };
 
   /**
-   * Given a native event, this function cancels it and dispatches it again from the host element using the desired
+   * @internal Given a native event, this function cancels it and dispatches it again from the host element using the desired
    * event options.
    */
   relayNativeEvent(event: Event, eventOptions?: EventInit) {
