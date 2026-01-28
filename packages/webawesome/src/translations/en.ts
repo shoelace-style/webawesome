b7b9ef731e4b6ea15b401cf74a75729bb1c8841a
Cory LaViska <cory@abeautifulsite.net>
2026-01-28 09:11:53 -0500

    update translations

--- packages/webawesome/src/translations/en.ts
+++ packages/webawesome/src/translations/en.ts
@@ -13,10 +13,12 @@ const translation: Translation = {
   copy: 'Copy',
   currentValue: 'Current value',
   dropFileHere: 'Drop file here or click to browse',
+  decrement: 'Decrement',
   dropFilesHere: 'Drop files here or click to browse',
   error: 'Error',
   goToSlide: (slide, count) => `Go to slide ${slide} of ${count}`,
   hidePassword: 'Hide password',
+  increment: 'Increment',
   loading: 'Loading',
   nextSlide: 'Next slide',
   numOptionsSelected: num => {
