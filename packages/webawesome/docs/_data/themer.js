f143016a8e44dd0d206fd2be7b65ef27e8fddf9e
lindsaym-fa <dev@lindsaym.design>
2026-07-01 13:51:57 -0400

    add Geist Mono italics to themer

--- packages/webawesome/docs/_data/themer.js
+++ packages/webawesome/docs/_data/themer.js
@@ -204,7 +204,7 @@ export const themes = [
       code: {
         name: 'Geist Mono',
         css: '"Geist Mono", monospace',
-        href: 'https://fonts.bunny.net/css?family=geist-mono:100,200,300,400,500,600,700,800,900&display=swap',
+        href: 'https://fonts.bunny.net/css?family=geist-mono:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap',
       },
       longform: {
         name: 'Aleo',
