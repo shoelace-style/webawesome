e6902fd2b000dd6247705e402b56286d647f06a9
zhangsixiao <zhangsixiao@bytedance.com>
2026-07-03 01:19:17 +0800

    Remove scroller aria-orientation test

--- packages/webawesome/src/components/scroller/scroller.test.ts
+++ packages/webawesome/src/components/scroller/scroller.test.ts
@@ -26,13 +26,6 @@ describe('<wa-scroller>', () => {
           const content = el.shadowRoot!.querySelector('#content')!;
           expect(content).to.have.attribute('aria-label');
         });
-
-        it('should not set aria-orientation on the region content container', async () => {
-          const el = await fixture<WaScroller>(html` <wa-scroller orientation="vertical"></wa-scroller> `);
-
-          const content = el.shadowRoot!.querySelector('#content')!;
-          expect(content).to.not.have.attribute('aria-orientation');
-        });
       });
 
       describe('properties', () => {
