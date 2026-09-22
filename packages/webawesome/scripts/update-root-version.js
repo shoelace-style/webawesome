357fdec091b2240fb516ad33b679f9a39b2cf22d
Konnor Rogers <konnor5456@gmail.com>
2026-09-22 11:52:14 -0400

    fix versions.txt version ordering

--- packages/webawesome/scripts/update-root-version.js
+++ packages/webawesome/scripts/update-root-version.js
@@ -3,6 +3,7 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 import * as url from 'url';
+import semver from "semver"
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
@@ -25,9 +26,7 @@ const versions = fs
   .readFileSync(versionsFile, { encoding: 'utf8' })
   .split(/\r?\n/)
   .filter(Boolean)
-  .sort((a, b) => {
-    return a.localeCompare(b);
-  });
+  .sort(semver.compare);
 
 if (!versions.includes(currentVersion)) {
   versions.push(currentVersion);
